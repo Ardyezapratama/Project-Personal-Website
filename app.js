970aa670aba2f3b23a1fe265fc519407f544cee3
Eza Prat <ardyezapratama@gmail.com>
2022-11-11 23:52:18 +0800

    Add Project

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+      });
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
